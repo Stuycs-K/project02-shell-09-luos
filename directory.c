f82bec71725bd755bce5ed0a4e2125dc970e5478
Steven Luo <sluo50@stuy.edu>
2024-12-04 13:48:27 -0500

    cd works

--- directory.c
+++ directory.c
@@ -1,13 +1,16 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 int notCD(char **arg_ary) {
-  if(strcmp(arg_ary[0], "cd")) {
-    chdir(arg_ary[1]);
-
-
+  if(strcmp(arg_ary[0], "cd") == 0) {
+    int number = chdir(arg_ary[1]);
+    if(number < 0) {
+        perror("cd failed\n");
+        exit(-1);
+    }
     return 1;
   }
   return 0;
